f6f92164d4d068e2ae3be8859ae44139dce1d442
cpuoverload <singhainanjifan@outlook.com>
2024-10-19 14:20:22 +0800

    feat: new nav and new routes

---
+++ src/pages/application/CreateApplication.tsx
@@ -0,0 +1,5 @@
+const Index: React.FC = () => {
+  return <div>create application</div>;
+};
+
+export default Index;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,18 +1,36 @@
 import { createBrowserRouter } from "react-router-dom";
+import { Group } from "@mantine/core";
+import {
+  IconApps,
+  IconBox,
+  IconChartPie,
+  IconCirclePlus,
+  IconFileCheck,
+  IconHistory,
+  IconHome,
+  IconListLetters,
+  IconTargetArrow,
+  IconUsers,
+} from "@tabler/icons-react";
 import RequireAuth from "./RequireAuth";
 import Layout from "@/components/Layout";
 import Login from "@/pages/user/Login";
 import Register from "@/pages/user/Register";
 import Home from "@/pages/Home";
+import CreateApplication from "@/pages/application/CreateApplication";
+import MyApplication from "@/pages/application/MyApplication";
+import MyAnswerRecord from "@/pages/answer-record/MyAnswerRecord";
 import UserManagement from "@/pages/management/UserManagement";
 
-interface Config {
+export interface Config {
   path?: string;
   element: JSX.Element;
   index?: boolean;
   children?: Config[];
   isNav?: boolean;
-  name?: string;
+  // isNav 为 true 时，需要指定遍历的 key
+  key?: string;
+  label?: React.ReactNode;
   role?: string[];
 }
 
@@ -30,7 +48,8 @@ const transformConfig = (config: Config[]) => {
 
     // 删除多余属性
     delete newRoute.isNav;
-    delete newRoute.name;
+    delete newRoute.key;
+    delete newRoute.label;
 
     // 如果有 role 属性，将 element 包裹在 ProtectedRoute 中
     if (newRoute.role) {
@@ -61,28 +80,130 @@ export const config: Config[] = [
         element: <Home />,
         index: true,
         isNav: true,
-        name: "Home",
+        key: "home",
+        label: (
+          <Group>
+            <IconHome />
+            <span>Home</span>
+          </Group>
+        ),
       },
       {
-        path: "/user-management",
+        path: "/application/create",
+        element: <CreateApplication />,
+        role: ["user", "admin"],
+        isNav: true,
+        key: "create-application",
+        label: (
+          <Group>
+            <IconCirclePlus />
+            <span>Create Application</span>
+          </Group>
+        ),
+      },
+      {
+        path: "/application/me",
+        element: <MyApplication />,
+        role: ["user", "admin"],
+        isNav: true,
+        key: "my-application",
+        label: (
+          <Group>
+            <IconBox />
+            <span>My Application</span>
+          </Group>
+        ),
+      },
+      {
+        path: "/answer-record/me",
+        element: <MyAnswerRecord />,
+        role: ["user", "admin"],
+        isNav: true,
+        key: "my-answer-record",
+        label: (
+          <Group>
+            <IconHistory />
+            <span>My Answer Record</span>
+          </Group>
+        ),
+      },
+      {
+        path: "/user/management",
         element: <UserManagement />,
         role: ["admin"],
         isNav: true,
-        name: "User Management",
+        key: "user-management",
+        label: (
+          <Group>
+            <IconUsers />
+            <span>User Management</span>
+          </Group>
+        ),
       },
       {
-        path: "/app-management",
-        element: <>app-management</>,
+        path: "/application/management",
+        element: <>application-management</>,
         role: ["admin"],
         isNav: true,
-        name: "App Management",
+        key: "application-management",
+        label: (
+          <Group>
+            <IconApps />
+            <span>Application Management</span>
+          </Group>
+        ),
       },
       {
-        path: "/question-management",
+        path: "/question/management",
         element: <>question-management</>,
         role: ["admin"],
         isNav: true,
-        name: "Question Management",
+        key: "question-management",
+        label: (
+          <Group>
+            <IconListLetters />
+            <span>Question Management</span>
+          </Group>
+        ),
+      },
+      {
+        path: "/scoring/management",
+        element: <>scoring-management</>,
+        role: ["admin"],
+        isNav: true,
+        key: "scoring-management",
+        label: (
+          <Group>
+            <IconTargetArrow />
+            <span>Scoring Management</span>
+          </Group>
+        ),
+      },
+      {
+        path: "/answer/management",
+        element: <>answer-management</>,
+        role: ["admin"],
+        isNav: true,
+        key: "answer-management",
+        label: (
+          <Group>
+            <IconFileCheck />
+            <span>Answer Management</span>
+          </Group>
+        ),
+      },
+      {
+        path: "/statistics",
+        element: <>statistics</>,
+        role: ["admin"],
+        isNav: true,
+        key: "statistics",
+        label: (
+          <Group>
+            <IconChartPie />
+            <span>Statistics</span>
+          </Group>
+        ),
       },
       {
         path: "*",
